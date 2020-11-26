71598818bdef855a2a8cd354f86441e4a9fd83cc
Irina <irinagmarques@hotmail.com>
2020-11-26 10:03:04 +0100

    Updates

---
+++ precache-manifest.03c3c61d0057016ca477f7f091e79b50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f67fa23755f250d278f8b04f0057ca86",
+    "url": "./index.html"
+  },
+  {
+    "revision": "44cc78a21cdc73dd9fa6",
+    "url": "./static/css/2.437d98bc.chunk.css"
+  },
+  {
+    "revision": "44cc78a21cdc73dd9fa6",
+    "url": "./static/js/2.5c0f8339.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "./static/js/2.5c0f8339.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60614d44b9104b3c1546",
+    "url": "./static/js/main.325b75fa.chunk.js"
+  },
+  {
+    "revision": "8cdb25f3ffb41440c4d8",
+    "url": "./static/js/runtime-main.e6c4d886.js"
+  }
+]);
